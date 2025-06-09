b17fab135e4873cc69f15fc4012163d90e69ea69
Isabell <igr45225@stud.hs-furtwangen.de>
2025-06-09 11:09:34 +0200

    abgabe physics

--- Projekt/character_body_3d.gd
+++ Projekt/character_body_3d.gd
@@ -1,6 +1,13 @@
 extends CharacterBody3D
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+
+
+@export var SPEED = 3.0
+@export var JUMP_VELOCITY = 4.5
+@export var camera_pan_speed = 1
+const PAN_SPEED_FACTOR = 0.00001
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _physics_process(delta: float) -> void:
@@ -14,7 +21,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("player_left", "player_right", "player_forward", "player_backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -22,5 +29,21 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		var mouse_vel = Input.get_last_mouse_velocity()
+		rotation.y -= mouse_vel.x * camera_pan_speed * PAN_SPEED_FACTOR
+		var cam_rig_rot = $camrearig.rotation.x + mouse_vel.y * camera_pan_speed * PAN_SPEED_FACTOR
+		cam_rig_rot = clamp(cam_rig_rot, -PI/2, -PI/8)
+		$camrearig.rotation.x = cam_rig_rot
+
+
+		if Input.is_action_just_pressed("ui_cancel"):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+	else:
+		if Input.is_action_just_pressed("ui_cancel"):
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 
 	move_and_slide()
